1a4b37ef3c5862e2ed2927e3b444556bda0a2a72
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-02-12 19:24:15 +0800

    Add Javalin 1.0.0

---
+++ src/main/external/1.0.0/Javalin.kt
@@ -0,0 +1,18 @@
+package javalin.performance
+
+import io.javalin.Javalin;
+
+fun main(args: Array<String>) {
+    Benchmarks.run(args)
+}
+
+open class JavalinBenchmark : HttpBenchmarkBase() {
+    var app :Javalin? = null
+    override fun startServer(port: Int) {
+       app =  Javalin.start(port)
+    }
+
+    override fun stopServer() {
+        app!!.stop()
+    }
+}
