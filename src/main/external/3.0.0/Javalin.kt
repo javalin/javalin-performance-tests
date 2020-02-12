436ed5a254e1513c74e7ced54ac9f3301ad7f9ae
vn7n24fzkq <vn7n24fzkq@gmail.com>
2020-02-12 19:00:01 +0800

    Make JMH can run with different version Javalin via gradle properties

---
+++ src/main/external/3.0.0/Javalin.kt
@@ -0,0 +1,18 @@
+package javalin.performance
+
+import io.javalin.Javalin;
+
+fun main(args: Array<String>) {
+    Benchmarks.run(args)
+}
+
+open class JavalinBenchmark : HttpBenchmarkBase() {
+    val app = Javalin.create();
+    override fun startServer(port: Int) {
+        app.start(port)
+    }
+
+    override fun stopServer() {
+        app.stop()
+    }
+}
